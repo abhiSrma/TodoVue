fb3282f31a4e9e77e1793417533526a96f4cf1e4
abhiSrma <abhi.921908@gmail.com>
2020-08-14 13:21:26 +0530

    Displaying basic todo list with data binding and v-for

---
+++ src/components/Todo.vue
@@ -0,0 +1,43 @@
+/* eslint-disable */
+<template>
+  <div>
+    <todo-list v-bind:todos='todos'></todo-list>
+  </div>
+</template>
+
+<script type = 'text/javascript' >
+import TodoList from './TodoList'
+export default {
+  components: {
+    TodoList
+  },
+  data () {
+    return {
+      todos: [
+        {
+          title: 'Todo A',
+          project: 'Project A',
+          done: false
+        },
+        {
+          title: 'Todo B',
+          project: 'Project B',
+          done: true
+        },
+        {
+          title: 'Todo C',
+          project: 'Project C',
+          done: false
+        },
+        {
+          title: 'Todo D',
+          project: 'Project D',
+          done: false
+        }
+      ]
+    }
+  }
+}
+</script>
+<style>
+</style>
